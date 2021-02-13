0176a36751ed1b22b76180f29a8c0532e2a1dedd
Allison Thackston <allison@allisonthackston.com>
2021-02-13 13:40:53 -0800

    get events from meetingplace.io on a schedule with a github action (#78)

--- assets/js/events-script.js
+++ assets/js/events-script.js
@@ -1,17 +1,17 @@
-function getCompareDate() { 
-    var d = new Date(), 
-        month = '' + (d.getMonth() + 1), 
-        day = '' + d.getDate(), 
-        year = d.getFullYear(); 
-    if (month.length < 2) month = '0' + month; 
-    if (day.length < 2) day = '0' + day; 
-    return [year, month, day].join(''); 
-  } 
-  
-  $('[future-date]').each(function() { 
-    if($(this).attr('future-date') <= getCompareDate()) $(this).hide(); 
-  });
+function getCompareDate() {
+  var d = new Date(),
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear();
+  if (month.length < 2) month = '0' + month;
+  if (day.length < 2) day = '0' + day;
+  return [year, month, day].join('');
+}
 
-  $('[past-date]').each(function() { 
-    if($(this).attr('past-date') > getCompareDate()) $(this).hide(); 
-  });
+$('[future-date]').each(function () {
+  if ($(this).attr('future-date') < getCompareDate()) $(this).hide();
+});
+
+$('[past-date]').each(function () {
+  if ($(this).attr('past-date') >= getCompareDate()) $(this).hide();
+});
