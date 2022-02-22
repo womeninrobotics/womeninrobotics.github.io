8fcf40acbc30b4057f780c652b9029a0775e01a1
Allison Thackston <allison@allisonthackston.com>
2022-02-21 23:58:47 -0800

    Update meetingplace script (#142)

---
+++ .github/meetingplace.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+declare -a Chapters=(
+    "womeninrobotics"
+    "women-in-robotics-bay-area"
+    "womeninroboticsmelbourne"
+    "women-in-robotics-boulder-denver"
+    "women-in-robotics-new-york"
+)
+
+# Get all chapter events
+for chapter in ${Chapters[@]}; do
+    curl -s "https://meetingplace.io/api/v1/group/${chapter}/events" | jq '.' > _data/wir_events_${chapter}.json
+done
+
+# Combine all chapter events
+jq -s '[.[][]]' _data/wir_events*.json > _data/events.json
+
+# Get all chapter past events
+for chapter in ${Chapters[@]}; do
+    curl -s "https://meetingplace.io/api/v1/group/${chapter}/past_events" | jq '.' > _data/wir_past_events_${chapter}.json
+done
+
+#Combine all past chapter events
+jq -s '[.[][]]' _data/wir_past_events*.json > _data/past_events.json
