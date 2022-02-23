f24b54593e5bd724346be28cfd5188b2bf7aa4c7
Allison Thackston <allison@allisonthackston.com>
2022-02-23 15:28:04 -0800

    Update to 2022 board

--- .github/htmlproofer.sh
+++ .github/htmlproofer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # BANDAID: http-status-ignore checks added due to rate limiting on social sites
-bundle exec htmlproofer --http-status-ignore 400,999 ./_site
+bundle exec htmlproofer --url-ignore "/www.twitter.com/,/www.linkedin.com/" ./_site
