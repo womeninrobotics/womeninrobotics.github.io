b99d6cd1a2863ac6edb705b03201feddf4e89d98
Allison Thackston <allison@allisonthackston.com>
2022-09-18 16:17:15 -0700

    Add bundler cache to test workflow (#178)

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,7 +2,7 @@
 
 # Install the version of Bundler.
 if [ -f Gemfile.lock ] && grep "BUNDLED WITH" Gemfile.lock > /dev/null; then
-    cat Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs gem install bundler -v
+    cat Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs sudo gem install bundler -v
 fi
 
 # If there's a Gemfile, then run `bundle install`
