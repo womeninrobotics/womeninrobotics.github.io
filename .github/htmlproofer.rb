b8db223d2013509b4068e1ed69b1905ce12a8f6a
Allison Thackston <allison@allisonthackston.com>
2023-05-09 12:00:38 -0700

    Add images for WiR List 2022 (#254)

--- .github/htmlproofer.rb
+++ .github/htmlproofer.rb
@@ -12,7 +12,8 @@ options = {
   ignore_urls: [
     "https://bootcamp.berkeley.edu/blog/free-stem-resources-for-girls-and-women-in-data-science-machine-learning-and-ai/",
     "https://bootcamp.pe.gatech.edu/blog/how-to-empower-girls-in-stem/",
-    /meetingplace.io/
+    /meetingplace.io/,
+    /twitter.com/
   ]
 }
 
