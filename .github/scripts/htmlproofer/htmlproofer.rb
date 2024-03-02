e21078faae655205e47bd1a061255cfb4aa39cc7
Allison Thackston <allison@allisonthackston.com>
2024-03-02 21:23:11 +0000

    Update default to use jekyll seo plugin for metadata

--- .github/scripts/htmlproofer/htmlproofer.rb
+++ .github/scripts/htmlproofer/htmlproofer.rb
@@ -16,11 +16,17 @@ options = {
     "https://bootcamp.berkeley.edu/blog/free-stem-resources-for-girls-and-women-in-data-science-machine-learning-and-ai/",
     "https://bootcamp.pe.gatech.edu/blog/how-to-empower-girls-in-stem/",
     /meetingplace.io/,
-    /twitter.com/
+    /twitter.com/,
+    "https://women-in-robotics.printify.me/product/2634428/women-in-robotics-day-2023-unisex-jersey-short-sleeve-tee",
+    "https://women-in-robotics.printify.me/product/2634433/women-in-robotics-day-2023-womens-favorite-tee"
   ],
-  ignore_status_codes: [ 
+  ignore_status_codes: [
     403
   ],
+  swap_urls: {
+    "https://womeninrobotics.org/" => "/",
+    "https://www.womeninrobotics.org/" => "/",
+  }
 }
 
 HTMLProofer.check_directory('./_site', options).run
