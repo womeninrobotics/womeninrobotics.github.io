2a340e6c23c8f9c227b34527ffe651b38caf0a1d
Allison Thackston <allison@allisonthackston.com>
2024-08-29 16:18:00 +0000

    Fix how csvjson is parsing google sheets

--- .github/actions/get-google-sheets-json/get_json.sh
+++ .github/actions/get-google-sheets-json/get_json.sh
@@ -3,7 +3,7 @@
 pip install csvkit
 # Get the spreadsheet as JSON
 curl -L "$SHEET_URL" \
-    | csvjson --indent 4 \
+    | csvjson --indent 4 -d , \
     | jq 'map(select(. | to_entries | map(.value != null) | any))' \
     >  $OUTPUT_FILE
 echo "Saved sheet to $OUTPUT_FILE."
