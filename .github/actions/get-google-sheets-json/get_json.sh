e2c341e6924d6b94980e30293b3617234ace3285
${GIT_USER_NAME} <${GIT_USER_EMAIL}>
2024-10-01 23:36:42 -0700

    Fix script to handle quoted entries with commas in google sheets csv

--- .github/actions/get-google-sheets-json/get_json.sh
+++ .github/actions/get-google-sheets-json/get_json.sh
@@ -2,8 +2,9 @@
 
 pip install csvkit
 # Get the spreadsheet as JSON
-curl -L "$SHEET_URL" \
-    | csvjson --indent 4 -d , \
-    | jq 'map(select(. | to_entries | map(.value != null) | any))' \
-    >  $OUTPUT_FILE
+curl -L "$SHEET_URL" |
+    csvformat -U 1 |
+    csvjson --indent 4 -d , |
+    jq 'map(select(. | to_entries | map(.value != null) | any))' \
+        >$OUTPUT_FILE
 echo "Saved sheet to $OUTPUT_FILE."
